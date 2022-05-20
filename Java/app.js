e9ac8ff1303d21b47d4b82505d7a86a20ff8975d
LucaSk-x <lucapiccinno64@gmail.com>
2022-05-20 13:06:53 +0200

    finito davvero

--- Java/app.js
+++ Java/app.js
@@ -1,37 +1,58 @@
-/* predere value da html
-creo un addEvent
-con i valori eseguo calcoli
-inserisco i dati e il risultato in html*/
+let userNameElement = document.querySelector("[name='userName']");
+let userKmElement = document.querySelector("[name='userKm']");
+let userAgeElement = document.querySelector("[name='userAge']");
+const generaBtn = document.getElementById("generaBtn");
 
-const billToKm = 0.21
-const discount20 = 0.8;
-const discount40 = 0.6;
+const kmPrice = 0.21;
+let scontoDaApplicare;
+let prezzoFinale;
 
+generaBtn.addEventListener('click', function() {
 
-const inputName = document.querySelector("name['userName']");
-const inputKm = document.querySelector("name['kmTrip']");
-const inputMinor = document.querySelector("value['Minorenne']");
-const inputAdult = document.querySelector("value['Maggiorenne']");
-const inputOver = document.querySelector("value['Over']");
-const submitButton = document.getElementById("submit");
+    userNameElement.value
+    userKmElement.value
+    userAgeElement.value
 
-/* Calcolo sconto */
+    let datiValidi = true;
 
-let totalPrice = billToKm * parseInt('inputKm')
+// validare/ controllare i dati inseriti
+if (isNaN(userAgeElement.value) || isNaN(userKmElement.value)) {
+    datiValidi = false;
+    alert("Devi inserire un numero valido per KM ed età");
+}
 
+// controllo che età sia maggiore di 14
+if (userAgeElement.value < 14) {
+    datiValidi = false;
+    alert("Sei troppo piccolo, torna a casa!");
+}
 
-submitButton.addEventListener("click", function () {
-    if (inputMinor) {
-        totalPrice = totalPrice * discount20 
-    } else if (inputOver) {
-        totalPrice = totalPrice * discount40
-    } else (inputAdult) {
-    
+// controllo che i km non siano superiori a 2000 e minori di 10
+if (userKmElement.value > 2000 || userKmElement.value < 10) {
+    datiValidi = false;
+    alert("I Km devono essere compresi tra 10 e 2000");
+}
+
+    if (datiValidi) {
+        prezzoFinale = kmPrice * userKmElement.value
     }
 
-    console.dir(inputKm)
-});
+    if (userAgeElement.value < 18) {
+        scontoDaApplicare = 0.80
+    } else if (userAgeElement.value > 65) {
+        scontoDaApplicare = 0.60
+    } else {
+        scontoDaApplicare = 1
+    }
 
+    prezzoFinale = scontoDaApplicare * prezzoFinale
 
+    let userName = document.querySelector('#userName');
+    userName.innerHTML = userNameElement.value
 
+    let userPrice = document.querySelector('#priceTicket');
+    userPrice.innerHTML = prezzoFinale
+
+    console.log(prezzoFinale)
+})
 
